9bcf7facc935a295ffa45bd638e793a6cbc1ab0f
Rahul <amaleexxii@gmail.com>
2024-09-08 12:18:35 +0530

    first commit

---
+++ public/user/assets/js/singleOrder.js
@@ -0,0 +1,27 @@
+async function statusUpdate(selectElement){
+    const productId = selectElement.getAttribute('data-product-id')
+    const mainProductId = selectElement.getAttribute('data-main-productId')
+    const productStatus = selectElement.value
+    const userId = document.getElementById('userId').value
+    const paymentMethod = document.getElementById('paymentMethod').value
+    const totalPrice = document.getElementById('totalPrice').value
+    try {
+        const response = await fetch('./single-order',{
+        method : 'post',
+        headers : {
+            "Content-Type" : "application/json"
+        },
+        body : JSON.stringify({ productStatus, productId, userId, paymentMethod, totalPrice , mainProductId })
+    })
+    if(!response){
+        throw new Error('something went wrong')
+    }
+    const data = await response.json()
+    if(data.message){
+        console.log('updated');
+    }
+    } catch (error) {
+        console.log(error);
+    }
+  
+}
---
+++ server/controller/user/products.js
@@ -0,0 +1,40 @@
+const Products = require("../../model/productmodel");
+
+const categoryProducts = async (req, res) => {
+  const { category } = req.params;
+  try {
+
+    // const limit = 20;
+    // let page = req.query.page || 1;
+    // const ProductCount = await Products.countDocuments({
+    //   selectedCategory: category,
+    // });
+
+    const products = await Products.find({ selectedCategory: category , deletedAt : 'Not-Deleted'});
+    console.log(products);
+    res.json(products);
+  } catch (error) {
+    console.log("error : ", error);
+  }
+};
+
+const search = async (req, res) => {
+  try {
+    const { search_text } = req.body;
+    const searchedProducts = await Products.find({
+      deletedAt: "Not-Deleted",
+      $or: [
+        { productName: { $regex: search_text, $options: "i" } },
+        { selectedCategory: { $regex: search_text, $options: "i" } },
+      ],
+    });
+    res.status(200).json({ searchedProducts });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+module.exports = {
+  categoryProducts,
+  search,
+};
